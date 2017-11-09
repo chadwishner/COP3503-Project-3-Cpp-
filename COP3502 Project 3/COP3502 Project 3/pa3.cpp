b0d858ea6e5b1b6c9bea0781ddebd34a026645a1
chadwishner <chadwishner@gmail.com>
2017-11-09 11:07:08 -0500

    Starting

---
+++ COP3502 Project 3/COP3502 Project 3/pa3.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COP3502 Project 3
+//
+//  Created by Chad Wishner on 11/9/17.
+//  Copyright © 2017 Chad Wishner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
---
+++ COP3502 Project 3/COP3502 Project 3/pa3.h
@@ -0,0 +1,13 @@
+//
+//  pa3.h
+//  COP3502 Project 3
+//
+//  Created by Chad Wishner on 11/9/17.
+//  Copyright © 2017 Chad Wishner. All rights reserved.
+//
+
+#ifndef pa3_h
+#define pa3_h
+
+
+#endif /* pa3_h */

3b92409990d30f8e965d328eee23993ef7569cbb
chadwishner <chadwishner@gmail.com>
2017-11-16 17:10:43 -0500

    Working 0

--- COP3502 Project 3/COP3502 Project 3/pa3.cpp
+++ COP3502 Project 3/COP3502 Project 3/pa3.cpp
@@ -1,15 +1,157 @@
-//
-//  main.cpp
-//  COP3502 Project 3
-//
-//  Created by Chad Wishner on 11/9/17.
-//  Copyright © 2017 Chad Wishner. All rights reserved.
-//
+// Chad Wishner
+// COP3502 Project 3
 
+
+#include "pa3.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+//string keywords[] = {"BEGIN", "END", "FOR"};
+//string operators[] = {"&&", "||", "=", "<", ">", "<=", ">=", "==","!","!=", "--", "++", "+", "-", "*", "/"};
+//string delimiters[] = {"(",")",",",";"};
+
+
+int paran = 0;
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	ifstream file;
+	string fileName = "";
+	
+	string line;
+	string nextExp;
+	
+	int depth = 0;
+	int paranthesis = 0;
+	
+	Stack* keywords = new Stack();
+	Stack* identifiers = new Stack();
+	Stack* constants = new Stack();
+	Stack* operators = new Stack();
+	Stack* Delimiters = new Stack();
+	Stack* errors = new Stack();
+	
+	cout << "Please enter the name of the input file: " << endl;
+	cin >> fileName;
+	
+	file.open(fileName);
+	if (!file) {
+		cout << "Unable to open file";
+		exit(1); // terminate with error
+	} else if (file.is_open()){
+		State state = FOR;
+		
+		while (file.good()){
+			file >> nextExp;
+			
+			int index;
+			string var;
+			
+			switch(state){
+				case FOR:
+					keywords -> push("FOR");
+					state = PARENTHESIS_VAR;
+					break;
+					
+				case PARENTHESIS_VAR:
+					index = 0;
+					var = "";
+					while (nextExp[index] == '(' || index != nextExp.length() - 1){
+							paranthesis ++;
+							if (index < nextExp.length() - 1){
+								index ++;
+							}
+						}
+					while (nextExp[index] !=  ',' || index != nextExp.length() - 1){
+						var += nextExp[index];
+						if (index < nextExp.length() - 1){
+							index ++;
+						}
+					}
+					identifiers -> push(var);
+					
+					state = CONSTANT;
+					break;
+					
+				case CONSTANT:
+					index = 0;
+					var = "";
+					while (nextExp[index] !=  ',' || index != nextExp.length() - 1){
+						var += nextExp[index];
+						if (index < nextExp.length() - 1){
+							index ++;
+						}
+					}
+					
+					constants -> push(var);
+					state = OPERATOR_PARENTHESIS;
+					break;
+					
+				case OPERATOR_PARENTHESIS:
+					index = 0;
+					if (nextExp.substr(0, 2) == "++"){
+						operators -> push("++");
+						index = 2;
+					} else if (nextExp.substr(0, 2) == "--"){
+						operators -> push("--");
+						index = 2;
+					}
+					while (nextExp[index] == ')' || index != nextExp.length() - 1){
+						paranthesis --;
+						if (index < nextExp.length() - 1){
+							index ++;
+						}
+					}
+					state = BEGIN;
+					break;
+					
+				case BEGIN:
+					if (nextExp == "BEGIN"){
+						keywords -> push("BEGIN");
+						depth ++;
+					} else {
+						errors -> push(nextExp);
+					}
+					state = FOR_OR_EXPRESS;
+					break;
+					
+				case FOR_OR_EXPRESS:
+					if (nextExp == "FOR"){
+						keywords -> push("FOR");
+						state = PARENTHESIS_VAR;
+					} else{
+						identifiers -> push(nextExp.substr(0, nextExp.find('=')));
+						state = PARENTHESIS_VAR;
+					}
+					break;
+					
+				case EXPRESS_OPERATOR:
+
+					break;
+				case VAR_CONSTANT:
+
+					break;
+				case FOR_OR_END:
+
+					break;
+				case END:
+
+					break;
+			}
+			//check for loops, if loop then check for begin, if begin then check for end
+			
+			
+		}
+	}
+	
+	
 	return 0;
 }
+
+//use stack to hold syntax errors
+//use stack to hold which keywords are used
+//use stack to hold identifiers
+//use stack to hold constants
+//use stack to hold operators
+//use stack to hold delimets
--- COP3502 Project 3/COP3502 Project 3/pa3.h
+++ COP3502 Project 3/COP3502 Project 3/pa3.h
@@ -1,13 +1,72 @@
-//
 //  pa3.h
 //  COP3502 Project 3
-//
-//  Created by Chad Wishner on 11/9/17.
-//  Copyright © 2017 Chad Wishner. All rights reserved.
-//
+
 
 #ifndef pa3_h
 #define pa3_h
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Stack {
+	private :	string array[100];
+				int top;
+	
+	public :	Stack();
+				void push (string var);
+				string pop ();
+				bool isEmpty();
+};
+
+Stack::Stack(){
+	top = -1;
+}
+
+void Stack::push (string var){
+	if (top == 99){
+		cout << "Stack is full" << endl;
+		return;
+	}
+	top++;
+	array[top] = var;
+}
+
+bool Stack::isEmpty(){
+	bool isTrue = true;
+	if (top == -1){
+		return false;
+	}
+	return isTrue;
+}
+
+string Stack::pop(){
+	string pop = array[top];
+	top--;
+	return pop;
+}
+
+enum State {
+	FOR,
+	PARENTHESIS_VAR,
+	CONSTANT,
+	OPERATOR_PARENTHESIS,
+	BEGIN,
+	FOR_OR_EXPRESS,
+	EXPRESS_OPERATOR,
+	VAR_CONSTANT,
+	FOR_OR_END,
+	END,
+};
+
+//int counthDepth(string line);
+//Stack findKeyWords(string line);
+//Stack findIdentifiers(string line);
+//Stack findConstants(string line);
+//Stack findOperators(string line);
+//Stack findDelimiters(string line);
+//Stack findSyntaxErrors(string line);
 
 
-#endif /* pa3_h */
+
+#endif pa3_h
